fac6b345ec1d1b4c92cea8529846123a8e8a287c
juliflorezg <forezjuli08@hotmail.com>
2023-01-12 15:44:26 -0500

    add services type model in schema

--- machines/todoAppMachine.ts
+++ machines/todoAppMachine.ts
@@ -2,25 +2,28 @@ import { createMachine } from 'xstate'
 export const todosMachine =
   /** @xstate-layout N4IgpgJg5mDOIC5QAsD2A3MAnABAWwEMBjZASwDswA6c1AFxzUy0gGIBZAeQFUBlAUU4A1fgCUA2gAYAuolAAHVLFJ1SqcnJAAPRAFoAbAA4ANCACeegEwBWKgHYALAE4nhgIz6AzDYeGnbgF8A0yZsfGIySipQlggOHgEeABUpWSQQRWVVdU0dBD8qQ2snTztrUwsEXQd9KmcXdy8fP0Cg01oIOE0Y8JIKME1MlTUNdLzdN09PCr03R3t6xu9rXyc2kB7CPqiYyEGlYZyxxGtLGYRJqmtJG7dDT31HazdJNeCNjDCtyOpaBl2IPssiNcnoiuddIZDFRLIsPMtVq0gkA */
 
-  /** @xstate-layout N4IgpgJg5mDOIC5QBcD2FUAIC2BDAxgBYCWAdmAHQAyquEZUmAKuqrAMQsayYA2tESAG0ADAF1EoAA5tiyYqlKSQAD0QBGABwiANCACeiALQBmAL5m9aDDgIly1AQ2asONOs+ttMAM1zFeYXFlGVg5BSUkVWN1AE4AFgoANnjNeJMAdgBWPUMEACYRC0sQUnQ4ZS9bIjIwENl5RWU1BCN8rKTk1PTs3Jis8xKqvBqHd3pSRi42erDGyNAWnINjJKyKLIsrVmr7SmmecchZ8KaolvUs2IoTLMuk9r6ES-UKdpERExE77IzNdXyWxAwzstUcHkmmC8PDAACdYahYSd5s1jIVNF00pllnl1PFEu9Pt91L9-oDikA */
+  /** @xstate-layout N4IgpgJg5mDOIC5QBcD2FUAIC2BDAxgBYCWAdmAHQAyquEZUmAKuqrAMQbkVkBuqAa0poMOAiW406DZq1gI+qfLmTFUpANoAGALradiUAAc2xVesMgAHogCMAdnsUAbLeeOAHAFYALPYDMXs7+HgA0IACeiAC0Pl4UPv7O3lr+AEzOWu4AnBkAvnnhIlh4RGSUUvSkjCwYHGAATg2oDRRGADYqAGYt2BTFYmWStFU1cgqk-Mrmmrr6liawZmqkljYI0bbZPi4+Hon2XuFRCGlaBYUgpOhwlgOlEmALpjNrMWlBu-v+h8fv9h4KNkPP5srktHsvEE0gUiqxBo9qCMZLU2M8lq8kNYYkkKGkMml-P5bD4fM5so4-hstrYEiCwT4tN5srYgj5YSB7uJyhRUbBMJVIOjlhYsetWdkKIFWc4PlTWbSPlpUlovA4vADbDDLlyhhVkdVMMV+Y1mg1hZjQOtomdAc49gcjpE7KS8V5lf5VerNdqCkA */
   createMachine(
     {
       id: 'todo machine',
       initial: 'Loading Todos',
       schema: {
-        events: {} as
-          | { type: 'Todos loaded'; todos: string[] }
-          | { type: 'Loading todos failed'; errorMessage: string },
+        // events: {} as
+        //   | { type: 'Todos loaded'; todos: string[] }
+        //   | { type: 'Loading todos failed'; errorMessage: string },
+        services: {} as {
+          loadTodos: {
+            data: string[]
+          }
+        },
       },
       tsTypes: {} as import('./todoAppMachine.typegen').Typegen0,
       states: {
         'Loading Todos': {
-          on: {
-            'Todos loaded': {
-              target: 'Todos Loaded',
-              actions: 'consoleLogTodos',
-            },
-            'Loading todos failed': 'Loading todos error',
+          invoke: {
+            src: 'loadTodos',
+            onDone: { target: 'Todos Loaded' },
+            onError: 'Loading todos error',
           },
         },
 
@@ -29,10 +32,10 @@ export const todosMachine =
       },
     },
     {
-      actions: {
-        consoleLogTodos: (context, event) => {
-          alert(JSON.stringify(event.todos))
-        },
-      },
+      // actions: {
+      //   consoleLogTodos: (context, event) => {
+      //     alert(JSON.stringify(event.todos))
+      //   },
+      // },
     }
   )
