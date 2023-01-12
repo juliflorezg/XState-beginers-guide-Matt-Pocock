05cf7fc68727b7abec1250fa080ed3319d11704b
juliflorezg <forezjuli08@hotmail.com>
2023-01-11 20:00:34 -0500

    add hover machine

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,30 @@
+import { useMachine } from '@xstate/react'
+import { NextPage } from 'next'
 import React from 'react'
+import { myMachine } from '../machines/myFirstMachine'
 
-export default function Home() {
-  return <div>HelloWorld</div>
+export const Home: NextPage = () => {
+  const [state, send] = useMachine(myMachine)
+
+  return (
+    <>
+      <div>{JSON.stringify(state.value)}</div>
+      <button
+        onClick={() => {
+          send('MOUSEOVER')
+        }}
+      >
+        Hover
+      </button>
+      <button
+        onClick={() => {
+          send('MOUSEOUT')
+        }}
+      >
+        Mouse out
+      </button>
+    </>
+  )
 }
+
+export default Home
