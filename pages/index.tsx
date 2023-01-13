b965848c10698f46e8c77ee02910df6495a36f79
juliflorezg <forezjuli08@hotmail.com>
2023-01-13 09:24:43 -0500

    add placeholder data for todos add services for save a todo & delete a todo, inside the useMachine hook

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,12 +4,24 @@ import React from 'react'
 // import { myMachine } from '../machines/myFirstMachine'
 import { todosMachine } from '../machines/todoAppMachine'
 
+const todos = new Set<string>(['do laundry', 'take bins out'])
+
 export const Home: NextPage = () => {
   const [state, send] = useMachine(todosMachine, {
     services: {
       loadTodos: async () => {
         // throw new Error('noooooooooooooo')
-        return ['do laundry', 'take bins out']
+        return Array.from(todos)
+      },
+      saveTodo: async (context, event) => {
+        todos.add(context.createNewTodoFormInput)
+      },
+      deleteTodo: async (context, event) => {
+        // const newTodosList = context.todos.filter(todo => todo !== context.todoToDelete)
+
+        // return newTodosList
+
+        todos.delete(context.todoToDelete as string)
       },
     },
   })
@@ -21,21 +33,58 @@ export const Home: NextPage = () => {
         <pre>{JSON.stringify(state.context)}</pre>
         <div>
           {state.matches('Todos Loaded') && (
-            <button
-              onClick={() => {
-                send({ type: 'Create new' })
+            <>
+              <button
+                onClick={() => {
+                  send({ type: 'Create new' })
+                }}
+              >
+                Create new
+              </button>
+              <button
+                onClick={() => {
+                  send({
+                    type: 'Delete todo',
+                  })
+                }}
+              >
+                Delete a todo
+              </button>
+            </>
+          )}
+          {state.matches('Deleting todo.Showing form input') && (
+            <form
+              onSubmit={e => {
+                e.preventDefault()
+                send({
+                  type: 'Submit',
+                })
               }}
             >
-              Create new
-            </button>
+              <input
+                type="text"
+                onChange={e => {
+                  send({ type: 'Form input changed', value: e.target.value })
+                }}
+              />
+            </form>
           )}
           {state.matches('Creating new todo.Showing form input') && (
-            <input
-              type="text"
-              onChange={e => {
-                send({ type: 'Form input changed', value: e.target.value })
+            <form
+              onSubmit={e => {
+                e.preventDefault()
+                send({
+                  type: 'Submit',
+                })
               }}
-            />
+            >
+              <input
+                type="text"
+                onChange={e => {
+                  send({ type: 'Form input changed', value: e.target.value })
+                }}
+              />
+            </form>
           )}
         </div>
       </div>
